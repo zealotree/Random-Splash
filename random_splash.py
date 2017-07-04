4aeffcfa03e7bb55933bce519885d6698f2486fd
Zealotree <zealotree@gmail.com>
2017-07-03 22:30:05 -0400

    Shortened default collections

--- random_splash.py
+++ random_splash.py
@@ -8,7 +8,7 @@ def getRandomcollection(collections):
     return random.choice(collections)
 
 CLIENT_ID = "" # Insert your Client ID HERE
-COLLECTION_IDS = ["306253,208422"]  # Choose a random photo from a list of collections
+COLLECTION_IDS = ["306253","208422"]  # Choose a random photo from a list of collections
 URL = "https://api.unsplash.com//photos/random?client_id={}&collections={}".format(CLIENT_ID, getRandomcollection(COLLECTION_IDS))
 
 
